3236ebb84aad77cfa549744d57b3a6863679e194
camdenhatch <camden.hatch@gmail.com>
2024-03-25 13:34:42 -0600

    more changes to js

--- stylesheet.js
+++ stylesheet.js
@@ -3,11 +3,26 @@
 // "make the navbar in the following code shrink when scrolling down 50px please:"
 
 window.onscroll = function () { scrollFunction() };
+window.onresize = function () { checkWindowSize() }; // Add this line
+
+// Call the function on initial load
+checkWindowSize();
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (window.innerWidth > 650) { // Check window width
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("navbarLogo").classList.add("shrink");
+        } else {
+            document.getElementById("navbarLogo").classList.remove("shrink");
+        }
+    }
+}
+
+function checkWindowSize() {
+    // If the window is less than 700px wide, add the 'shrink' class; otherwise, run scrollFunction to determine if it needs to be added or removed based on scroll position
+    if (window.innerWidth <= 650) {
         document.getElementById("navbarLogo").classList.add("shrink");
     } else {
-        document.getElementById("navbarLogo").classList.remove("shrink");
+        scrollFunction(); // This will ensure the correct class is applied based on the scroll position when the window is resized
     }
 }
